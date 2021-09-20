f989cbac730e68a53e1caafbb2bef59634566c8c
KSRAHUL <sairahul99@yahoo.in>
2021-09-19 21:03:04 -0400

    Test comment

--- src/main/java/DBConnectionTester.java
+++ src/main/java/DBConnectionTester.java
@@ -2,17 +2,17 @@ import java.sql.*;
 
 public class DBConnectionTester {
 
-
-    public static void main(String args[]) {
+   //this a test to see if Github integration works.
+    public static void main(String[] args) {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/spike01","root","SQL6112@99");
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from games");
-            while(resultSet.next()){
-                System.out.println(resultSet.getString("gameName"));
-            }
+            //ResultSet resultSet = statement.executeQuery("select * from games");
+            //while(resultSet.next()){
+             //   System.out.println(resultSet.getString("gameName"));
+           // }
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
